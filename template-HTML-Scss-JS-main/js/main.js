242ba2392419645601080d64ca06cc2916f120f1
shuto1019 <fuse.shuto1019@gmail.com>
2025-05-17 13:46:20 +0900

    feat: create accordion

---
+++ template-HTML-Scss-JS-main/js/main.js
@@ -0,0 +1,30 @@
+const accordionBtns = document.querySelectorAll('.accordionBtn');
+
+const removeActive = () => {
+  const activeContents = document.querySelectorAll('.active');
+  activeContents.forEach(activeContent => {
+    activeContent.classList.remove('active');
+    activeContent.style.height = null;
+  });
+};
+
+const addActive = (accordionContent, ul) => {
+  accordionContent.classList.add('active');
+  ul.classList.add('active');
+  ul.style.height = ul.scrollHeight + 'px'; 
+};
+
+accordionBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const accordionContent = btn.parentElement;
+    const ul = accordionContent.querySelector('ul');
+    const isAlreadyActive = ul.classList.contains('active');
+
+    if(isAlreadyActive){
+        removeActive();
+    }
+    if (!isAlreadyActive) {
+      addActive(accordionContent, ul);
+    }
+  });
+});
