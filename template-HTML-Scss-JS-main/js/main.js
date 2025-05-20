246d462b3077fc1cc0d4f6d6f5334b92332259a9
shuto1019 <fuse.shuto1019@gmail.com>
2025-05-20 21:12:09 +0900

    feat: implement accordion UI with smooth toggle animation

--- template-HTML-Scss-JS-main/js/main.js
+++ template-HTML-Scss-JS-main/js/main.js
@@ -3,13 +3,12 @@ const accordionBtns = document.querySelectorAll('.accordionBtn');
 const removeActive = () => {
   const activeContents = document.querySelectorAll('.active');
   activeContents.forEach(activeContent => {
+    activeContent.style.height = "0px";
     activeContent.classList.remove('active');
-    activeContent.style.height = null;
   });
 };
 
-const addActive = (accordionContent, ul) => {
-  accordionContent.classList.add('active');
+const addActive = (ul) => {
   ul.classList.add('active');
   ul.style.height = ul.scrollHeight + 'px'; 
 };
@@ -17,14 +16,13 @@ const addActive = (accordionContent, ul) => {
 accordionBtns.forEach(btn => {
   btn.addEventListener('click', () => {
     const accordionContent = btn.parentElement;
-    const ul = accordionContent.querySelector('ul');
-    const isAlreadyActive = ul.classList.contains('active');
-
-    if(isAlreadyActive){
-        removeActive();
+    const ul               = accordionContent.querySelector('ul');
+    if(ul.classList.contains('active')) {
+      removeActive();
+    }else {
+      removeActive();
+      addActive(ul);ß
     }
-    if (!isAlreadyActive) {
-      addActive(accordionContent, ul);
-    }
+    
   });
 });
